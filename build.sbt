bb0cdabcdf0d709f7847fd0c049d926c64ac5dcc
zxt <bad58n81so@126.com>
2018-05-11 14:47:18 +0800

    basic message

--- build.sbt
+++ build.sbt
@@ -1,6 +1,7 @@
 name := "leanakka"
 
-version := "0.1"
+version := "0.1-SNAPSHOT"
+organization :="com.akkademy-db"
 
 scalaVersion := "2.12.2"
 
@@ -9,5 +10,10 @@ lazy val akkaVersion = "2.5.12"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
   "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+  "com.typesafe.akka" %% "akka-remote" % akkaVersion,
   "org.scalatest" %% "scalatest" % "3.0.1" % "test"
 )
+
+mappings in (Compile, packageBin) ~= {_.filterNot { case (_, name) =>
+    Seq("application.conf").contains(name)
+}}
--- src/main/scala/com/akkademy/AkkademyDB.scala
+++ src/main/scala/com/akkademy/AkkademyDB.scala
@@ -1,8 +1,8 @@
 package com.akkademy
 
-import akka.actor.Actor
+import akka.actor.{Actor, Status}
 import akka.event.Logging
-import com.akkademy.message.SetRequest
+import com.akkademy.message.{GetRequest, KeyNotFoundException, SetRequest}
 
 import scala.collection.mutable
 
@@ -14,7 +14,19 @@ class AkkademyDB extends Actor{
     case SetRequest(key,value) =>{
       log.info("Received SetRequest - key: {} value: {}",key,value)
       map.put(key,value)
+      sender() ! Status.Success
     }
-    case o =>  log.info("Received unknown message: {}",o)
+    case GetRequest(key) =>{
+      log.info("Received GetRequest - key {}",key)
+      val respons :Option[Object] = map.get(key)
+      respons match {
+        case Some(x) => sender() ! x
+        case None    => sender() ! Status.Failure(new KeyNotFoundException(key))
+      }
+    }
+    case o =>{
+      log.info("Received unknown message: {}",o)
+      Status.Failure(new ClassNotFoundException())
+    }
   }
 }
